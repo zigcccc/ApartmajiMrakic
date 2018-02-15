e0108b5d2a1ccc0ce3bfdbd35f6237c89ba246ae
Žiga Krašovec <zkrasovec@gmail.com>
2018-02-15 10:34:03 +0100

    Updated mobile styles

--- wordpress/wp-content/cache/autoptimize/js/autoptimize_snippet_ff7c0ceecd310a6a418efee270b14595.js
+++
@@ -1,2 +0,0 @@
-jQuery(document).ready(function(){var slider_height=jQuery('.slider-slides').height();var fadeUntil=slider_height/1,paddingFactor=slider_height/5;jQuery(window).bind('scroll',function(){var offset=jQuery(document).scrollTop(),opacity=0,padding=0;if(offset<=fadeUntil){opacity=1-offset/fadeUntil;padding=paddingFactor*offset/fadeUntil;}
-jQuery('.slide-body').css('margin-top',padding);});});
